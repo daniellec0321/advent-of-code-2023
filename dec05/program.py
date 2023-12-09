9c3d7ecb2589a66bfdeb40034a4b642b2bebd598
Danielle Croft <daniellec0321@gmail.com>
2023-12-09 13:29:08 -0500

    Dec05: This is hard

--- dec05/program.py
+++ dec05/program.py
@@ -24,7 +24,49 @@ def puzzle1():
 
 
 def puzzle2():
-    pass
+    lines = [x.rstrip() for x in list(filter(lambda l: l != '\n', sys.stdin.readlines()))]
+    seed_vals = list(map(int, list(filter(lambda l: l != '', \
+            lines[0].rstrip().split(':')[1].rstrip().split(' ')))))
+    seeds = set()
+    updated = dict()
+    for i in range(0, len(seed_vals), 2):
+        seeds.add((seed_vals[i], seed_vals[i+1]))
+    new_seeds = set()
+    for line in lines[1:]:
+        # print(f'at line {line}')
+        if '-' in line:
+            seeds.update(new_seeds)
+            new_seeds = set()
+            continue
+        dst_start = int(line.split(' ')[0])
+        src_start = int(line.split(' ')[1])
+        range_amount = int(line.split(' ')[2])
+        src_end = src_start + range_amount
+        copy = seeds.copy()
+        # for seed in seeds:
+        #     if 1:
+        #     # if (src_start <= seed <= src_end) and (not updated[(seed)]):
+        #         pass
+        #     else:
+        #        new_seeds.add(seed)
+        for seed in copy:
+            # seeds.remove(seed)
+            # new_seeds.add(seed)
+            seed_start = seed[0]
+            seed_end = seed[0] + seed[1]
+            if (src_start <= seed_start < src_end):
+                print(f'for line {line} and seed {seed}, need to update')
+                seeds.remove(seed)
+                # Split range into two tuples
+                # If the entire seed is affected
+                affected = (seed_start, seed_end)
+                if seed_end > src_end:
+                    affected = (69, 69)
+                    # unaffected equals something that we need to put into seeds
+            elif (src_start < seed_end <= src_end):
+                pass
+
+    # print(f'The answer to puzzle 2 is {min(seeds)}')
 
 
 
