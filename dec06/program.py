05ee26fe6e45ad6fe1eac23eb397e6b5bc8cbd22
Danielle Croft <daniellec0321@gmail.com>
2023-12-10 14:02:31 -0500

    Dec06: Done with puzzle 1

--- dec06/program.py
+++ dec06/program.py
@@ -1,2 +1,35 @@
-if __name__ == '__main__':
+import sys
+
+def puzzle1():
+    times = list(map(int, filter(lambda l: l != '', \
+            sys.stdin.readline().rstrip().split(':')[1].rstrip().split(' '))))
+    distances = list(map(int, filter(lambda l: l != '', \
+            sys.stdin.readline().rstrip().split(':')[1].rstrip().split(' '))))
+    product = 1
+    for time, best_distance in zip(times, distances):
+        ways_to_win = 0
+        for button_hold in range(1, time):
+            time_left = time-button_hold
+            distance_traveled = time_left*button_hold
+            if (distance_traveled > best_distance):
+                ways_to_win += 1
+        product *= ways_to_win
+    print(f'The answer to puzzle 1 is {product}')
+
+
+
+def puzzle2():
     pass
+
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 2 or (sys.argv[1] != '1' and sys.argv[1] != '2'):
+        print('Please include \'1\' or \'2\' to indicate which puzzle to test.')
+        sys.exit(1)
+
+    if sys.argv[1] == '1':
+        puzzle1()
+    elif sys.argv[1] == '2':
+        puzzle2()
