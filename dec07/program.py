b25e7a00f00e02b781b4f0f42edbe5219adb0cc5
Danielle Croft <daniellec0321@gmail.com>
2023-12-10 15:33:09 -0500

    Dec07: Done

--- dec07/program.py
+++ dec07/program.py
@@ -1,56 +1,53 @@
 import sys
 from collections import Counter
-# CARDS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-CARDS = 'AKQJT98765432'
-
-def find_score(hand: str) -> int:
-    # Check for five of a kind
-    counts = Counter(hand)
-    if len(counts) == 1:
-        return 6
-    # Check for four of a kind
-    elif (len(counts) == 2) and (4 in counts.values()):
-        return 5
-    # Check for full house
-    elif (len(counts) == 2) and (3 in counts.values()):
-        return 4
-    # Check for three of a kind
-    elif (len(counts) == 3) and (3 in counts.values()):
-        return 3
-    # Check for two pair
-    elif (len(counts) == 3):
-        return 2
-    elif (len(counts) == 4) and (2 in counts.values()):
-        return 1
-    else:
-        return 0
+from itertools import product
+CARDS = 'AKQT98765432J'
     
 class Hand:
+
     def __init__(self, hand: str):
         self.hand = hand
         self.score = self.find_score(hand)
 
+    def findOccurrences(self, string: str, ch: str) -> list[int]:
+        return [i for i, letter in enumerate(string) if letter == ch]
+
     def find_score(self, hand: str) -> int:
         # Check for five of a kind
-        counts = Counter(hand)
-        if len(counts) == 1:
-            return 6
-        # Check for four of a kind
-        elif (len(counts) == 2) and (4 in counts.values()):
-            return 5
-        # Check for full house
-        elif (len(counts) == 2) and (3 in counts.values()):
-            return 4
-        # Check for three of a kind
-        elif (len(counts) == 3) and (3 in counts.values()):
-            return 3
-        # Check for two pair
-        elif (len(counts) == 3):
-            return 2
-        elif (len(counts) == 4) and (2 in counts.values()):
-            return 1
-        else:
-            return 0
+        jokers = self.findOccurrences(hand, 'J')
+        max_score = -1
+        tests = product(CARDS, repeat=len(jokers))
+        for test in tests:
+            # Create new "hand"
+            new_hand = list(hand)
+            for idx, j in enumerate(jokers):
+                new_hand[j] = test[idx]
+            counts = Counter(new_hand)
+            if len(counts) == 1:
+                return 6
+            # Check for four of a kind
+            elif (len(counts) == 2) and (4 in counts.values()):
+                # return 5
+                max_score = max(max_score, 5)
+            # Check for full house
+            elif (len(counts) == 2) and (3 in counts.values()):
+                # return 4
+                max_score = max(max_score, 4)
+            # Check for three of a kind
+            elif (len(counts) == 3) and (3 in counts.values()):
+                # return 3
+                max_score = max(max_score, 3)
+            # Check for two pair
+            elif (len(counts) == 3):
+                # return 2
+                max_score = max(max_score, 2)
+            elif (len(counts) == 4) and (2 in counts.values()):
+                # return 1
+                max_score = max(max_score, 1)
+            else:
+                # return 0
+                max_score = max(max_score, 0)
+        return max_score
 
     
     def __eq__(self, other):
@@ -102,7 +99,17 @@ def puzzle1():
 
 
 def puzzle2():
-    pass
+    hands = list()
+    hands_to_bids = dict()
+    for line in sys.stdin.readlines():
+        hand = Hand(line.split(' ')[0].rstrip())
+        hands.append(hand)
+        hands_to_bids[hand.hand] = int(line.split(' ')[1].rstrip())
+    hands.sort(reverse=True)
+    total_winnings = 0
+    for idx, hand in enumerate(hands):
+        total_winnings += ((len(hands)-idx) * hands_to_bids[hand.hand])
+    print(f'The answer to puzzle 2 is {total_winnings}')
 
 
 
