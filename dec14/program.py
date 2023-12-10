b5f2ff4f27fc4da0a91d0a5e3ad73a2d88781ba6
Danielle Croft <daniellec0321@gmail.com>
2023-12-10 14:25:38 -0500

    Created better templates

--- dec14/program.py
+++ dec14/program.py
@@ -1,2 +1,22 @@
-if __name__ == '__main__':
+import sys
+
+def puzzle1():
     pass
+
+
+
+def puzzle2():
+    pass
+
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 2 or (sys.argv[1] != '1' and sys.argv[1] != '2'):
+        print('Please include \'1\' or \'2\' to indicate which puzzle to test.')
+        sys.exit(1)
+
+    if sys.argv[1] == '1':
+        puzzle1()
+    elif sys.argv[1] == '2':
+        puzzle2()
