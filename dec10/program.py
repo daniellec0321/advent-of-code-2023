f5abd2b16fb4a2051d8704132f2a0da42dda4e23
Danielle Croft <daniellec0321@gmail.com>
2023-12-12 01:06:48 -0500

    Dec10: Started part 2

--- dec10/program.py
+++ dec10/program.py
@@ -73,7 +73,84 @@ def puzzle1():
 
 
 def puzzle2():
-    pass
+    
+    # helper function that finds a spot
+    def find_initial_spots(start_index: tuple) -> tuple:
+        s1 = start_index
+        s2 = start_index
+        moves = {'north': (-1,0,), 'east': (0,1), 'south': (1,0), 'west': (0,-1)}
+        for move, coord in moves.items():
+            spot = tuple(np.add(start_index, coord))
+            # Check that coord is in grid
+            if (spot[0] < 0) or (spot[1] < 0) or (spot[0] >= len(grid)) or (spot[1] >= len(grid[0])):
+                continue
+            # Check if this is a valid connection
+            symbol = grid[spot[0]][spot[1]]
+            if (move == 'north' and ((symbol == '|') or (symbol == '7') or (symbol == 'F'))) or \
+                (move == 'east' and ((symbol == '-') or (symbol == '7') or (symbol == 'J'))) or \
+                (move == 'south' and ((symbol == '|') or (symbol == 'J') or (symbol == 'L'))) or \
+                (move == 'west' and ((symbol == '-') or (symbol == 'L') or (symbol == 'F'))):
+                if s1 == start_index:
+                    s1 = spot
+                else:
+                    s2 = spot
+                    break
+        return (s1, s2)
+
+    def find_spot(curr_index: tuple, came_from: tuple) -> tuple:
+        symbol = grid[curr_index[0]][curr_index[1]]
+        moves = []
+        if symbol == '-':
+            moves = [tuple(np.add(curr_index, (0,-1)))] + [tuple(np.add(curr_index, (0,1)))]
+        elif symbol == '|':
+            moves = [tuple(np.add(curr_index, (-1,0)))] + [tuple(np.add(curr_index, (1,0)))]
+        elif symbol == 'L':
+            moves = [tuple(np.add(curr_index, (-1,0)))] + [tuple(np.add(curr_index, (0,1)))]
+        elif symbol == 'J':
+            moves = [tuple(np.add(curr_index, (-1,0)))] + [tuple(np.add(curr_index, (0,-1)))]
+        elif symbol == '7':
+            moves = [tuple(np.add(curr_index, (1,0)))] + [tuple(np.add(curr_index, (0,-1)))]
+        elif symbol == 'F':
+            moves = [tuple(np.add(curr_index, (1,0)))] + [tuple(np.add(curr_index, (0,1)))]
+        for move in moves:
+            if move != came_from:
+                return move
+        return (-1, -1)
+
+    # Read in grid
+    grid = [line.strip() for line in sys.stdin.readlines()]
+    route = dict()
+    # Find index of starting position
+    start_index = (-1,-1)
+    for row, line in enumerate(grid):
+        for idx, symbol in enumerate(line):
+            if symbol == 'S':
+                start_index = (row, idx)
+                route[(row,idx)] = 'r'
+            else:
+                route[(row,idx)] = 'n'
+    # Initialize spots
+    s1, s2 = find_initial_spots(start_index)
+    route[s1] = 'r'
+    route[s2] = 'r'
+    s1_came_from = start_index
+    s2_came_from = start_index
+    while s1 != s2:
+        s1_new = find_spot(s1, s1_came_from)
+        s2_new = find_spot(s2, s2_came_from)
+        s1_came_from = s1
+        s2_came_from = s2
+        s1 = s1_new
+        s2 = s2_new
+        route[s1] = 'r'
+        route[s2] = 'r'
+    # Now that route is found, go around route again and find the inside
+    curr_spot = start_index
+    curr_spot, s2 = find_initial_spots(start_index)
+    while curr_spot != start_index:
+        break
+
+    print(f'The answer to puzzle 2 is uh')
 
 
 
