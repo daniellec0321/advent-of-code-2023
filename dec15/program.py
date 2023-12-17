88322e1aa6a222081e3ab6be88813ac3da84af7e
Danielle Croft <daniellec0321@gmail.com>
2023-12-17 16:17:21 -0500

    Dec15: Done

--- dec15/program.py
+++ dec15/program.py
@@ -16,8 +16,63 @@ class Puzzle1():
 
 
 class Puzzle2():
+
+    def hash(self, string: str) -> int:
+        current_value = 0
+        for char in string:
+            current_value += ord(char)
+            current_value *= 17
+            current_value %= 256
+        return current_value
+
     def solve_puzzle(self, stream=sys.stdin):
-        pass
+
+        boxes = dict() # Key: integer representing the box number. # Value: a list of tuples
+        for elem in sys.stdin.readline().strip().split(','):
+            label = ''
+            op = '='
+            focal_length = -1
+            if '-' in elem:
+                label = elem.split('-')[0]
+                op = '-'
+            else:
+                label = elem.split('=')[0]
+                focal_length = int(elem.split('=')[1])
+            box_number = self.hash(label)
+            if op == '-':
+                # check for box number
+                if box_number in boxes:
+                    found = -1
+                    for idx, t in enumerate(boxes[box_number]):
+                        if label == t[0]:
+                            found = idx
+                            break
+                    if found != -1:
+                        del boxes[box_number][found]
+                    # Check for empty dictionary
+                    if len(boxes[box_number]) == 0:
+                        del boxes[box_number]
+            # need to do the = thing
+            else:
+                if not box_number in boxes:
+                    boxes[box_number] = [(label, focal_length)]
+                else:
+                    # see if box already exists
+                    found = False
+                    for idx, t in enumerate(boxes[box_number]):
+                        if t[0] == label:
+                            found = True
+                            boxes[box_number][idx] = (label, focal_length)
+                            break
+                    if not found:
+                        boxes[box_number].append((label, focal_length))
+        
+        # Add up all of the values in boxes
+        sum = 0
+        for key, value in boxes.items():
+            for idx, t in enumerate(value):
+                sum += (key+1)*(idx+1)*(t[1])
+        print(f'The answer to puzzle 2 is {sum}')
 
 
 
