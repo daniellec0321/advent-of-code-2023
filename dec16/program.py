b7e876a96cc5178fae3b011613827d54822b1590
Danielle Croft <daniellec0321@gmail.com>
2023-12-17 16:31:45 -0500

    Dec16: Started puzzle 1

--- dec16/program.py
+++ dec16/program.py
@@ -1,8 +1,31 @@
 import sys
 
 class Puzzle1():
+
     def solve_puzzle(self, stream=sys.stdin):
-        pass
+        # Read in grid
+        self.grid = [list(x.strip()) for x in stream.readlines()]
+        self.dp = set() # tuples with coordinates, direction
+        self.energized = set() # a set of coordinates that are energized
+        self.run_beam((0,0), 'right')
+
+    def run_beam(self, start_location: tuple[int, int], direction: str):
+        # Check if this beam has already been run
+        if (start_location, direction) in self.dp:
+            return
+        
+        # add this start location to energized
+        self.energized.add(start_location)
+        
+        # Get list of where the beam is going
+        track = list()
+        if direction == 'right':
+            track = self.grid[start_location[0]][start_location[1]+1:]
+        elif direction == 'left':
+            track = self.grid[start_location[0]][:start_location[1]]
+            track.reverse()
+        elif direction == 'down':
+            pass
 
 
 
