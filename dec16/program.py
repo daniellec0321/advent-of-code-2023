31691999dcc759873635099979f7b533c508782a
Danielle Croft <daniellec0321@gmail.com>
2023-12-18 01:15:25 -0500

    Dec16: Puzzle 1 done

--- dec16/program.py
+++ dec16/program.py
@@ -1,4 +1,5 @@
 import sys
+from queue import Queue
 
 class Puzzle1():
 
@@ -18,53 +19,118 @@ class Puzzle1():
     def addt(self, t1, t2):
         return tuple([sum(x) for x in zip(t1, t2)])
 
-    def run_beam(self, start_location: tuple[int, int], direction: str):
+    def run_beam(self, start_location: tuple[int, int], start_direction: str):
+
+        # Add this to the queue
+        q = Queue()
+        q.put((start_location, start_direction))
+
+        while not q.empty():
+
+            # Pop from the queue
+            spot, direction = q.get()
+            # print(spot, direction)
+            x, y = spot
+            
+            # Check if in range or in dp
+            if not (0 <= x < len(self.grid)) or not (0 <= y < len(self.grid[0])):
+                continue
+            if (spot, direction) in self.dp:
+                continue
+
+            # add to energized and dp
+            self.energized.add(spot)
+            self.dp.add((spot, direction))
+
+            char = self.grid[x][y]
+
+            # Go through cases
+            if char == '|' and ((direction == 'left') or (direction == 'right')):
+                up_spot = self.addt(spot, self.directions['up'])
+                down_spot = self.addt(spot, self.directions['down'])
+                q.put((up_spot, 'up'))
+                q.put((down_spot, 'down'))
+            elif char == '-' and ((direction == 'up') or (direction == 'down')):
+                left_spot = self.addt(spot, self.directions['left'])
+                right_spot = self.addt(spot, self.directions['right'])
+                q.put((right_spot, 'right'))
+                q.put((left_spot, 'left'))
+            elif char == '/':
+                if direction == 'right':
+                    new_spot = self.addt(spot, self.directions['up'])
+                    q.put((new_spot, 'up'))
+                elif direction == 'left':
+                    new_spot = self.addt(spot, self.directions['down'])
+                    q.put((new_spot, 'down'))
+                elif direction == 'up':
+                    new_spot = self.addt(spot, self.directions['right'])
+                    q.put((new_spot, 'right'))
+                else:
+                    new_spot = self.addt(spot, self.directions['left'])
+                    q.put((new_spot, 'left'))
+            elif char == '\\':
+                if direction == 'right':
+                    new_spot = self.addt(spot, self.directions['down'])
+                    q.put((new_spot, 'down'))
+                elif direction == 'left':
+                    new_spot = self.addt(spot, self.directions['up'])
+                    q.put((new_spot, 'up'))
+                elif direction == 'up':
+                    new_spot = self.addt(spot, self.directions['left'])
+                    q.put((new_spot, 'left'))
+                else:
+                    new_spot = self.addt(spot, self.directions['right'])
+                    q.put((new_spot, 'right'))
+            else:
+                new_spot = self.addt(spot, self.directions[direction])
+                # print(f'new spot is {new_spot}, direction is {direction}')
+                q.put((new_spot, direction))
 
         # Check if beam out of range
-        if start_location[0] < 0 or start_location[1] < 0 or \
-        start_location[0] >= len(self.grid) or start_location[1] >= len(self.grid[0]):
-            return
+        # if start_location[0] < 0 or start_location[1] < 0 or \
+        # start_location[0] >= len(self.grid) or start_location[1] >= len(self.grid[0]):
+        #     return
         
-        print(start_location, direction)
+        # print(start_location, direction)
 
-        # Check if this beam has already been run
-        if (start_location, direction) in self.dp:
-            return
+        # # Check if this beam has already been run
+        # if (start_location, direction) in self.dp:
+        #     return
         
-        # add this start location to energized and dp
-        self.energized.add(start_location)
-        self.dp.add((start_location, direction))
+        # # add this start location to energized and dp
+        # self.energized.add(start_location)
+        # self.dp.add((start_location, direction))
 
-        # get locations to recurse to
-        char = self.grid[start_location[0]][start_location[1]]
+        # # get locations to recurse to
+        # char = self.grid[start_location[0]][start_location[1]]
 
-        if char == '|' and ((direction == 'left') or (direction == 'right')):
-            self.run_beam(self.addt(start_location, self.directions['up']), 'up')
-            self.run_beam(self.addt(start_location, self.directions['down']), 'down')
-        elif char == '-' and ((direction == 'down') or (direction == 'up')):
-            self.run_beam(self.addt(start_location, self.directions['right']), 'right')
-            self.run_beam(self.addt(start_location, self.directions['left']), 'left')
-        elif char == '/':
-            if direction == 'up':
-                self.run_beam(self.addt(start_location, self.directions['right']), 'right')
-            elif direction == 'down':
-                self.run_beam(self.addt(start_location, self.directions['left']), 'left')
-            elif direction == 'left':
-                self.run_beam(self.addt(start_location, self.directions['down']), 'down')
-            else:
-                self.run_beam(self.addt(start_location, self.directions['up']), 'up')
-        elif char == '\\':
-            if direction == 'up':
-                self.run_beam(self.addt(start_location, self.directions['left']), 'left')
-            elif direction == 'down':
-                self.run_beam(self.addt(start_location, self.directions['right']), 'right')
-            elif direction == 'left':
-                self.run_beam(self.addt(start_location, self.directions['up']), 'up')
-            else:
-                self.run_beam(self.addt(start_location, self.directions['down']), 'down')
-        else:
-            # to_go.append(self.addt(start_location, self.directions[direction]))
-            self.run_beam(self.addt(start_location, self.directions[direction]), direction)
+        # if char == '|' and ((direction == 'left') or (direction == 'right')):
+        #     self.run_beam(self.addt(start_location, self.directions['up']), 'up')
+        #     self.run_beam(self.addt(start_location, self.directions['down']), 'down')
+        # elif char == '-' and ((direction == 'down') or (direction == 'up')):
+        #     self.run_beam(self.addt(start_location, self.directions['right']), 'right')
+        #     self.run_beam(self.addt(start_location, self.directions['left']), 'left')
+        # elif char == '/':
+        #     if direction == 'up':
+        #         self.run_beam(self.addt(start_location, self.directions['right']), 'right')
+        #     elif direction == 'down':
+        #         self.run_beam(self.addt(start_location, self.directions['left']), 'left')
+        #     elif direction == 'left':
+        #         self.run_beam(self.addt(start_location, self.directions['down']), 'down')
+        #     else:
+        #         self.run_beam(self.addt(start_location, self.directions['up']), 'up')
+        # elif char == '\\':
+        #     if direction == 'up':
+        #         self.run_beam(self.addt(start_location, self.directions['left']), 'left')
+        #     elif direction == 'down':
+        #         self.run_beam(self.addt(start_location, self.directions['right']), 'right')
+        #     elif direction == 'left':
+        #         self.run_beam(self.addt(start_location, self.directions['up']), 'up')
+        #     else:
+        #         self.run_beam(self.addt(start_location, self.directions['down']), 'down')
+        # else:
+        #     # to_go.append(self.addt(start_location, self.directions[direction]))
+        #     self.run_beam(self.addt(start_location, self.directions[direction]), direction)
 
 class Puzzle2():
     def solve_puzzle(self, stream=sys.stdin):
