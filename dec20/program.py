65ad0a4d187d45e081b40ef4306a3d2a4c536115
Danielle Croft <daniellec0321@gmail.com>
2024-01-06 16:56:38 -0500

    Dec20: Part 2 bad approach

--- dec20/program.py
+++ dec20/program.py
@@ -142,8 +142,102 @@ class Puzzle1():
 
 
 class Puzzle2():
+
+    class flipflop():
+        def __init__(self):
+            self.state = 'off'
+            self.outputs = list()
+        def rec_pulse(self, pulse: str, inp=None) -> str:
+            if pulse == 'low' and self.state == 'off':
+                self.state = 'on'
+                return 'high'
+            if pulse == 'low' and self.state == 'on':
+                self.state = 'off'
+                return 'low'
+            return ''
+
+    class conjunction():
+        def __init__(self):
+            self.inputs = dict() # Key: string of input, Value: string of high or low
+            self.outputs = list()
+        def rec_pulse(self, pulse: str, inp: str) -> str:
+            self.inputs[inp] = pulse
+            if all([x == 'high' for x in self.inputs.values()]):
+                return 'low'
+            return 'high'
+        
+    class broadcaster():
+        def __init__(self):
+            self.outputs = list()
+        
+    def is_flipflop(self, obj):
+        return isinstance(obj, Puzzle1.flipflop)
+    
+    def is_conjunction(self, obj):
+        return isinstance(obj, Puzzle1.conjunction)
+    
+    def is_broadcaster(self, obj):
+        return isinstance(obj, Puzzle1.broadcaster)
+
     def solve_puzzle(self, stream=sys.stdin):
-        pass
+        
+        # Read in input
+        vars = dict() # Key: string of the label. Value: the object
+        for line in stream.readlines():
+            source = line.split(' -> ')[0]
+            outputs = [x.strip() for x in line.split(' -> ')[1].split(', ')]
+            if source[0] == '%':
+                new_obj = self.flipflop()
+                new_obj.outputs = outputs
+                vars[source[1:]] = new_obj
+            elif source[0] == '&':
+                new_obj = self.conjunction()
+                new_obj.outputs = outputs
+                vars[source[1:]] = new_obj
+            else:
+                new_obj = self.broadcaster()
+                new_obj.outputs = outputs
+                vars[source] = new_obj
+
+        # Set up the inputs of all of the conjunctions
+        for label, var in vars.items():
+            for op in var.outputs:
+                if op in vars and self.is_conjunction(vars[op]):
+                    vars[op].inputs[label] = 'low'
+
+        # Loop through pressing the button 1000 times
+        # for i in range(1, 1001):
+        i = 1
+        while True:
+            # Queue will be a list of tuples (module, pulse, source)
+            if i % 1000 == 0:
+                print(f'On cycle {i}')
+            low_to_rx = 0
+            high_to_rx = 0
+            q = Queue()
+            for op in vars['broadcaster'].outputs:
+                q.put((op, 'low', 'broadcaster'))
+                if op == 'rx':
+                    low_to_rx += 1
+            while not q.empty():
+                curr_label, curr_pulse, pulse_from = q.get()
+                out_pulse = ''
+                if curr_label in vars:
+                    out_pulse = vars[curr_label].rec_pulse(curr_pulse, pulse_from)
+                # Add this stuff to the queue
+                if out_pulse:
+                    for op in vars[curr_label].outputs:
+                        q.put((op, out_pulse, curr_label))
+                        if op == 'rx' and out_pulse == 'high':
+                            high_to_rx += 1
+                        elif op == 'rx' and out_pulse == 'low':
+                            low_to_rx += 1
+            if low_to_rx == 1 and high_to_rx == 0:
+                break
+            i += 1
+
+        # Answer needs to be greater than 1000
+        print(f'The answer to puzzle 2 is {i}')
 
 
 
