f695028b92476cd51045cfbf5c8f915abd2edbcc
Danielle Croft <daniellec0321@gmail.com>
2024-01-06 17:18:36 -0500

    Dec21: started

--- dec21/program.py
+++ dec21/program.py
@@ -1,8 +1,33 @@
 import sys
+from queue import Queue
 
 class Puzzle1():
+
+    def addt(t1: tuple, t2: tuple) -> tuple:
+        return tuple([sum(x) for x in zip(t1, t2)])
+
     def solve_puzzle(self, stream=sys.stdin):
-        pass
+        # Read in map
+        grid = list()
+        for idx, line in enumerate(stream.readlines()):
+            grid.append(line.strip())
+            if 'S' in line:
+                start_point = (idx, line.find('S'))
+
+        frontier = Queue() # tuples of (x, y, cycle)
+        visited = set() # tuples of (x, y, cycle)
+        end_spots = set() # tuples of (x, y)
+        frontier.put((*start_point, 0))
+        visited.add((*start_point, 0))
+
+        moves = [(-1,0), (1,0), (0,-1), (0,1)]
+        
+        while not frontier.empty:
+            x, y, curr_cycle = frontier.get()
+            if curr_cycle == 64:
+                end_spots.add((x, y))
+                continue
+            # Add on possible spots to queue
 
 
 
